ea41cf7a1275e7aefdf9bcffabecb5277d4331eb
root <root@ip-172-26-15-136.us-west-2.compute.internal>
2019-10-20 21:33:44 +0000

    Port to Beyondcoin

--- scripts/peers.js
+++ scripts/peers.js
@@ -32,8 +32,9 @@ mongoose.connect(dbString, function(err) {
             // peer already exists
             loop.next();
           } else {
-            request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
-              db.create_peer({
+            // request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
+            request({uri: 'https://1stbitco.in/api/?ip=' + address, json: true}, function (error, response, geo) {
+	      db.create_peer({
                 address: address,
                 protocol: body[i].version,
                 version: body[i].subver.replace('/', '').replace('/', ''),
